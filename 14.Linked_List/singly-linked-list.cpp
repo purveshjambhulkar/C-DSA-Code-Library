bf757777379ca9d0b9281ff759853257d9469824
purveshjambhulkar <purveshjambhulkar16@gmail.com>
2023-11-26 13:05:22 +0530

    new commit

--- 14.Linked_List/singly-linked-list.cpp
+++ 14.Linked_List/singly-linked-list.cpp
@@ -16,10 +16,10 @@ public:
         this->next = NULL;
     }
 };
-//function to find the length (will define later)
-int Length_linkedlist(Node* &head);
+// function to find the length (will define later)
+int Length_linkedlist(Node *&head);
 
-//function to head at head
+// function to head at head
 void InsertionAtHead(int data, Node *&head)
 {
 
@@ -28,7 +28,7 @@ void InsertionAtHead(int data, Node *&head)
     head = temp;
 }
 
-//function to insert at tail
+// function to insert at tail
 void InsertionAtTail(int data, Node *&tail)
 {
     Node *temp = new Node(data);
@@ -36,24 +36,18 @@ void InsertionAtTail(int data, Node *&tail)
     tail = temp;
 }
 
-//function to insert a given position
-void InsertionAtPositon(int position , int data , Node* &head , Node* &tail){
-
-
-    Node* temp = head ; 
-    int count = 1 ;
+// function to insert a given position
+void InsertionAtPositon(int position, int data, Node *&head, Node *&tail)
+{
 
     if (position == 1)
     {
-        InsertionAtHead(data , head);
+        InsertionAtHead(data, head);
+        return;
     }
 
-    if (position == Length_linkedlist(head))
-    {
-        InsertionAtTail(data ,tail);
-    }
-    
-    
+    Node *temp = head;
+    int count = 1;
 
     while (count < position - 1)
     {
@@ -61,29 +55,41 @@ void InsertionAtPositon(int position , int data , Node* &head , Node* &tail){
         count++;
     }
 
-    Node* NodeToBeInserted =  new Node(data);
-    NodeToBeInserted->next = temp->next;
-    temp->next = NodeToBeInserted;    
-    
+    // if (position == Length_linkedlist(head))   //AMBIGUITY//
+    // {
+    //     InsertionAtTail(data ,tail);
+    //     return;
+    // }
 
+    // OR
+    // if the element is to be addded at last position
+    if (temp->next == NULL)
+    {
+        InsertionAtTail(data, tail);
+        return;
+    }
+
+    Node *NodeToBeInserted = new Node(data);
+    NodeToBeInserted->next = temp->next;
+    temp->next = NodeToBeInserted;
 }
 
-//defining the function to calculate length of linked list
-int Length_linkedlist(Node* &head){
+// defining the function to calculate length of linked list
+int Length_linkedlist(Node *&head)
+{
     int length = 0;
-    Node* temp = head ;
+    Node *temp = head;
     while (temp != NULL)
     {
-        temp = temp->next ;
+        temp = temp->next;
         length++;
     }
 
-    return length ; 
-    
+    return length;
 }
 
-//function to iterate and print the linked list
-void print(Node *&head )
+// function to iterate and print the linked list
+void print(Node *&head)
 {
     Node *ptr = head;
     while (ptr != NULL)
@@ -94,26 +100,22 @@ void print(Node *&head )
     cout << endl;
 }
 
-
-
 int main()
 {
 
     Node *node1 = new Node(10);
     Node *head = node1; // starting of the node
     Node *tail = node1; // ending of the node
-    
-    
-    //InsertionAtHead(15 , head);
-    
+
+    // InsertionAtHead(15 , head);
 
     InsertionAtTail(25, tail);
     InsertionAtTail(35, tail);
-    cout<<"Printing after insertion in Tial\n";
-    print(head );
-    InsertionAtPositon(3 , 99 , head , tail);
-    cout<<"Printing after insertion in Position\n";
-    print(head );
-    
-
+    cout << "Printing after insertion in Tail\n";
+    print(head);
+    InsertionAtPositon(4, 99, head, tail);
+    cout << "Printing afheter insertion in Position\n";
+    print(head);
+    cout << "head : " << head->data << endl;
+    cout << "tail : " << tail->data << endl;
 }
